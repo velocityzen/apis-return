9600667fdef24b60297e5759e23544ec2a0dae9a
velocityzen <velocityzen@gmail.com>
2015-10-02 15:31:41 -0400

    fixed logic. added editorconfig

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-"use strict";
-var errors = require("apis/lib/errors");
+'use strict';
+var errors = require('apis/lib/errors');
 var isEmpty = function(something) {
-	if(!something) {
-		return true;
-	}
+  if (!something) {
+    return true;
+  }
 
-	return typeof something === "object" && !Object.keys(something).length;
+  return typeof something === 'object' && !Object.keys(something).length;
 };
 
 errors.handler = function(ErrorClass, retName, cb) {
-	if(typeof ErrorClass === "string") {
-		ErrorClass = errors[ErrorClass];
-	}
+  if (typeof ErrorClass === 'string') {
+    ErrorClass = errors[ErrorClass];
+  }
 
-	if(typeof retName === "function") {
-		cb = retName;
-		retName = undefined;
-	}
+  if (typeof retName === 'function') {
+    cb = retName;
+    retName = undefined;
+  }
 
-	return function(err, result) {
-		if(err) {
-			cb(new ErrorClass(err.message));
-		} else if(isEmpty(result) && ErrorClass) {
-			cb(new ErrorClass());
-		} else if(retName === undefined) {
-			cb(null, result);
-		} else {
-			var ret = {};
-			if(typeof retName === "string") {
-				ret[retName] = result;
-			} else {
-				ret = retName;
-			}
-			cb(null, ret);
-		}
-	};
+  return function(err, result) {
+    if (err) {
+      cb(new ErrorClass(err.message));
+    } else if (retName !== undefined) {
+      var ret = {};
+      if (typeof retName === 'string') {
+        ret[retName] = result;
+      } else {
+        ret = retName;
+      }
+      cb(null, ret);
+    } if (isEmpty(result) && ErrorClass) {
+      cb(new ErrorClass());
+    } else {
+      cb(null, result);
+    }
+  };
 };
 
 module.exports = errors;
